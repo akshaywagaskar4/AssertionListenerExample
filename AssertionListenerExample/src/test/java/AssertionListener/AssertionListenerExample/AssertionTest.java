a33b20ee40d37971e25d969303c29bccbed15807
akshaywagaskar4 <69782210+akshaywagaskar4@users.noreply.github.com>
2021-10-03 19:04:02 +0530

    commit first

--- AssertionListenerExample/src/test/java/AssertionListener/AssertionListenerExample/AssertionTest.java
+++ AssertionListenerExample/src/test/java/AssertionListener/AssertionListenerExample/AssertionTest.java
@@ -25,7 +25,7 @@ public class AssertionTest {
   @BeforeClass
   public void openBrowser() {
 	  
-
+//velocity
 		System.setProperty(key, value);
 		 d = new ChromeDriver();
 		 d.manage().window().maximize();
